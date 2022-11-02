3dfb901683cc9e1d662bfa3eb138754dd8821ba4
unknown <oskarlietzau1@gmail.com>
2022-11-02 22:25:09 +0100

    service

---
+++ src/main/java/com/crud/task/controller/TaskController.java
@@ -0,0 +1,26 @@
+package com.crud.task.controller;
+
+import com.crud.task.domain.Task;
+import com.crud.task.domain.TaskDto;
+import com.crud.task.mapper.TaskMapper;
+import com.crud.task.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
+    @GetMapping
+    public List<TaskDto> getTasks() {
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
+    }
+}
---
+++ src/main/java/com/crud/task/domain/Task.java
@@ -0,0 +1,23 @@
+package com.crud.task.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name")
+    private String title;
+
+    @Column(name = "description")
+    private String content;
+}
